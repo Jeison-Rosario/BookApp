b9cc1d351de22a9d9104c3068d6ba9aa390942d3
Jeison Rosario <jsrosario97@gmail.com>
2025-03-31 20:03:53 -0400

    sexto commit

--- context/appContext.js
+++ context/appContext.js
@@ -1,9 +1,16 @@
-const { Sequelize } = require('sequelize');
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
 
-const connection = new Sequelize('BookApp', 'root', '', {
-    dialect: 'mysql',
-    host: 'localhost',
-    port: 3306
-});
+const connection = new Sequelize(
+  process.env.MYSQLDATABASE, // Nombre de la base de datos
+  process.env.MYSQLUSER,     // Usuario
+  process.env.MYSQLPASSWORD, // Contraseña
+  {
+    dialect: "mysql",
+    host: process.env.MYSQLHOST, // Host (Railway)
+    port: process.env.MYSQLPORT, // Puerto (Railway)
+    logging: false, 
+  }
+);
 
 module.exports = connection;
